400a377ab6405d684f7e8f9555f9369d510ad481
Lol4ik <dania.tenditnyi@gmail.com>
2026-09-26 14:55:14 +0300

    Fix whitenoise strict mode for Tailwind

--- ai_manager/ai_manager/settings.py
+++ ai_manager/ai_manager/settings.py
@@ -212,3 +212,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+WHITENOISE_MANIFEST_STRICT = False
